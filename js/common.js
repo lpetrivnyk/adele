069de526f9b29edb89c6f911e6d560a996e46a52
ukrainian199 <lubomyr.petrivnyk@gmail.com>
2017-01-20 22:42:51 +0200

    The problem with tabs!

--- js/common.js
+++ js/common.js
@@ -1,25 +1,15 @@
-/* The script for the phone */
-$(document).ready(function(){
-    $('.lg_phone').click(function(){
-        $(this).parent().children('.ph_number').slideToggle();
-        return false;
+$( ".cross" ).hide();
+$( ".menu" ).hide();
+$( ".hamburger" ).click(function() {
+    $( ".menu" ).slideToggle( "slow", function() {
+        $( ".hamburger" ).hide();
+        $( ".cross" ).show();
     });
-    var mobile = (/iphone|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase()));
-    if (mobile) {
-        $('span.ph_number').css('display', 'none');
-    }
 });
 
-/* Script adaptive menu */
-$(document).ready(function(){
-    $("#nav-mobile").html($("#nav-main").html());
-    $("#nav-trigger span").click(function(){
-        if ($("nav#nav-mobile ul").hasClass("expanded")) {
-            $("nav#nav-mobile ul.expanded").removeClass("expanded").slideUp(250);
-            $(this).removeClass("open");
-        } else {
-            $("nav#nav-mobile ul").addClass("expanded").slideDown(250);
-            $(this).addClass("open");
-        }
+$( ".cross" ).click(function() {
+    $( ".menu" ).slideToggle( "slow", function() {
+        $( ".cross" ).hide();
+        $( ".hamburger" ).show();
     });
 });
